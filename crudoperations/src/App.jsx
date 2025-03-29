64629d18ff4c99928e903e69b8fb2b465f1ac8da
Mayur Rastogi <mayurrastogi46@gmail.com>
2025-03-29 19:46:05 +0530

    add records in file

--- crudoperations/src/App.jsx
+++ crudoperations/src/App.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
@@ -8,17 +8,30 @@ function App() {
     phone: "",
     password: "",
   });
+  const [users, setUsers] = useState([]);
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setUser({ ...user, [name]: value });
   };
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(user);
+    setUsers([...users, user]);
+    setUser({
+      name: "",
+      email: "",
+      phone: "",
+      password: "",
+    });
   };
+
+  useEffect(() => {
+    console.log(users);
+  }, [users]);
   return (
     <div div className="container">
       <div className="form">
+        <h1>Creation Of Records</h1>
         <form onSubmit={handleSubmit}>
           <input
             type="text"
@@ -61,18 +74,29 @@ function App() {
       </div>
 
       <div className="table">
-      <table>
-        <thead>
+        <h1>Records</h1>
+        <table>
+          <thead>
+            <tr>
+              <td>S.no</td>
+              <td>Name</td>
+              <td>Email</td>
+              <td>Phone</td>
+              <td>Password</td>
+            </tr>
+          </thead>
+          <tbody>{users.map((item, index) => (
+            <tr>
+              <td>{index+1}</td>
+              <td>{item.name}</td>
+              <td>{item.email}</td>
+              <td>{item.phone}</td>
+              <td>{item.password}</td>
 
-          
-        </thead>
+            </tr>
 
-
-
-
-      </table>
-    
-
+          ))}</tbody>
+        </table>
       </div>
     </div>
   );
