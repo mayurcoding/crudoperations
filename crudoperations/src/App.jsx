d5b2ffb5f6b36edc409bff69083ad545d9025523
Mayur Rastogi <mayurrastogi46@gmail.com>
2025-03-29 19:22:09 +0530

    create form

--- crudoperations/src/App.jsx
+++ crudoperations/src/App.jsx
@@ -1,9 +1,64 @@
+import React, {useState} from "react";
 import "./App.css";
 
 function App() {
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    password: "",
+  });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(user);
+  };
   return (
     <>
-     <h1>Hello</h1>
+      <div className="form">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            value={user.name}
+            onChange={handleChange}
+          />
+          <br />
+          <br />
+          <input
+            type="text"
+            placeholder="Email"
+            name="email"
+            value={user.email}
+            onChange={handleChange}
+          />
+          <br />
+          <br />
+          <input
+            type="text"
+            placeholder="Phone Number"
+            name="phone"
+            value={user.phone}
+            onChange={handleChange}
+          />
+          <br />
+          <br />
+          <input
+            type="text"
+            placeholder="Password"
+            name="password"
+            value={user.password}
+            onChange={handleChange}
+          />
+          <br />
+          <br />
+          <button>Save</button>
+        </form>
+      </div>
     </>
   );
 }
