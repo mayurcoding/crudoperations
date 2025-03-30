de1a8bdb4a99cebc1899b61eb5b8c5150df4e20d
Mayur Rastogi <mayurrastogi46@gmail.com>
2025-03-30 10:37:55 +0530

    create slices in redux

---
+++ crudoperations/src/Redux/Slice/Api/State/userSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = { value: 0 }
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {},
+});
+
+export const { } = userSliceSlice.actions;
+export default userSlice.reducer
--- crudoperations/src/main.jsx
+++ crudoperations/src/main.jsx
@@ -2,12 +2,20 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+
+//Context
 import UserContextProvider from "./Context/UserContextProvider.jsx";
+
+//Redux
+import { Provider } from "react-redux";
+import { store } from "./Redux/Store/store.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <UserContextProvider>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </UserContextProvider>
   </StrictMode>
 );
